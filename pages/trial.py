18dd3294935632eb6141d61154afc557f2b6053c
gladys mawarni <gladysmawarni@gmail.com>
2024-09-24 19:19:07 +0100

    trial

--- pages/trial.py
+++ pages/trial.py
@@ -1,36 +1,22 @@
 import streamlit as st
-
+import requests
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
 import time
+
 
 
 st.title('test')
 
-def ss_website(url):
-    driver = None
-    try:
-        # Using on Local
-        options = webdriver.ChromeOptions()
-        options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        options.add_argument('--window-size=1920,1200')
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),
-                                  options=options)
-        st.write(f"DEBUG:DRIVER:{driver}")
-        driver.get(url)
-        time.sleep(5)
-        driver.save_screenshot('screenie.png')
-        st.image('screenie.png')
-        driver.quit()
-        
-        
-    except Exception as e:
-        st.write(f"DEBUG:INIT_DRIVER:ERROR:{e}")
-    finally:
-        if driver is not None: driver.quit()
-    return None
-
 url = 'https://www.sevenrooms.com/explore/thetamilcrown/reservations/create/search?venues=thetamilcrown%2Cthetamilprince'
-ss_website(url)
+# test = requests.get()
+# print(test.content)
+
+options = Options()
+options.add_argument("--headless")
+browser = webdriver.Firefox()
+browser.get(url)
+time.sleep(3)
+browser.save_screenshot('screenie.png')
+st.image('screenie.png')
+browser.close()
